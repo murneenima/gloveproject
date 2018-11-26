fae02611c94a75c7a6fd3229e0617dea815ead05
Murnee Nima (Top Glove Medical – F07) <murnee@topglove.tg.local>
2018-11-26 10:19:58 +0700

    signin / signup

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 
 var Schema = mongoose.Schema
 
+//-------------------------------Staff Schema --------------------------------------------//
 var StaffSchema = new Schema({
     badgeNo: {
         type: String,
@@ -32,7 +33,25 @@ var StaffSchema = new Schema({
         required: true
     }
 })
+
+//------------------------------- Admin Schema -------------------------------------------//
+var AdminSchema = new Schema({
+    username:{
+      type:String,
+      required:true,
+      unique:true 
+    },
+    password:{
+        type:String,
+        required:true,
+        unique:true,
+        minlength:8
+    }
+})
+
+
 var Staff = mongoose.model('Staff', StaffSchema)
+var Admin = mongoose.model('Admin', AdminSchema)
 
 mongoose.connect('mongodb://localhost:27017/dbase').then((doc) => {
     console.log('@@@@ Success to connect with Database @@@')
@@ -44,11 +63,48 @@ mongoose.connect('mongodb://localhost:27017/dbase').then((doc) => {
 var app = express()
 app.use(bodyParser.json())
 
+//-------------------------------------------------------------------------------------------//
 app.get('/',(req,res)=>{
     res.send('hello')
 })
 
 
+
+//----------------------------------- Sign Up-----------------------------------------------//
+app.post('/signup',(req,res)=>{
+    let newAdmin = Admin({
+        username : req.body.username,
+        password : req.body.password
+    })
+
+    newAdmin.save().then((doc)=>{
+        res.send(doc)
+    },(err) => {
+        res.status(400).send(err)
+    })
+})
+
+
+
+//------------------------------------- Sign in --------------------------------------------//
+app.get('/signin',(req,res)=>{
+    let usernameInput = req.headers['username']
+    let passwordInput = req.headers['password']
+
+    Admin.find({
+        username:usernameInput,
+        password:passwordInput
+    }).then((admin)=>{
+        if(admin.length==1){
+            res.send(admin[0])
+        }else if(admin.length == 0){
+            res.status(400).send('Login Error')
+        }
+    },(err)=>{
+        res.status(400).send(err)
+    })
+
+})
 
 
 
