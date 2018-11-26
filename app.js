6da0da51b9f833a918eebcd89d8475b6d13091c6
Murnee Nima (Top Glove Medical – F07) <murnee@topglove.tg.local>
2018-11-26 17:45:32 +0700

    finished login logout and add staff data

--- app.js
+++ app.js
@@ -14,23 +14,23 @@ var StaffSchema = new Schema({
         required: true,
         minlength: 7
     },
-    name: {
+    emp_name: {
         type: String,
         required: true
     },
-    surname: {
+    emp_surname: {
         type: String,
         required: true
     },
-    position: {
+    emp_position: {
         type: String,
         required: true
     },
-    dept: {
+    emp_dept: {
         type: String,
         required: true
-    },
-    status: {
+    }, 
+    emp_status: {
         type: String,
         required: true
     }
@@ -63,8 +63,6 @@ mongoose.connect('mongodb://localhost:27017/dbase').then((doc) => {
 
 
 
-
-
 //-------------------------------------------------------------------------------------------//
 var app = express()
 app.use(bodyParser.json())
@@ -72,8 +70,10 @@ app.use(bodyParser.urlencoded({
     extended:true
 }));
 
+app.set('view engine', 'hbs');
+app.use(express.static('public'))
 
-//-------------------------------------------------------------------------------------------//
+//---------------------------------- API test------------------------------------------------//
 app.get('/',(req,res)=>{
     res.send('hello')
 })
@@ -118,7 +118,84 @@ app.post('/signin',(req,res)=>{
 
 })
 
-// ------------------------------ Port -------------------------------------------------------
+
+//-------------------------------------- Add Staff -----------------------------------------//
+app.post('/addstaff',(req,res)=>{
+
+    // ------ Position --------
+    if(req.body.emp_position == 'Choose'){
+        res.status(400).send('Position doesnot choose');
+           return
+    }
+
+    if(req.body.emp_dept == 'Choose'){
+        res.status(400).send('Department doesnot choose');
+           return
+    }
+
+    if(req.body.emp_status == 'Choose'){
+        res.status(400).send('Status doesnot choose');
+           return
+    }
+
+    let emp_position  = ' '
+    if(req.body.emp_position == 'Excutive'){
+        emp_position= 'Excutive';
+    }else if(req.body.emp_position == 'Excutive II'){
+        emp_position= 'Excutive II';
+    }else if(req.body.emp_position == 'Officer'){
+        emp_position= 'Officer';
+    }else if(req.body.emp_position == 'Supervisor'){
+        emp_position = 'Supervisor';
+    }else if(req.body.emp_position == 'Admin/Secretary'){
+        emp_position = 'Admin/Secretary'
+    }
+    //console.log('Position == ',req.body.emp_position)
+
+
+    //-------- Department -------------
+    
+    let emp_dept = ' '
+    if(req.body.emp_dept == 'Admin'){
+        emp_dept = 'Admin';
+    }else if(req.body.emp_dept == 'HR'){
+        emp_dept = 'HR';
+    }else if(req.body.emp_dept == 'Finance'){
+        emp_dept = 'Finance';
+    }else if(req.body.emp_dept == 'IT'){
+        emp_dept = 'IT';
+    }
+    //console.log('Dept == ',req.body.emp_dept)
+
+    //------------------ Status ---------
+   
+    let emp_status =' '
+    if(req.body.emp_status){
+        emp_status = 'Online';
+    }else if(req.body.emp_status){
+        emp_status = 'AL';
+    }
+    //console.log('Status == ',req.body.emp_status)
+
+    //---------------------------------
+    let newStaff = Staff({
+        badgeNo :req.body.badgeNo,
+        emp_name : req.body.emp_name,
+        emp_surname : req.body.emp_surname,
+        emp_position:emp_position,
+        emp_dept:emp_dept,
+        emp_status:emp_status
+    })
+
+
+    newStaff.save().then((doc)=>{
+        res.render('admin_staff.hbs')
+    },(err)=>{
+        res.status(400).send(err)
+    })
+})
+
+//------------------------------- Port -----------------------------------------------------//
 app.listen(3000,()=>{
-    console.log(' ##### listening on port 3000 ####')
+    console.log(' ##### listening on port 3000 #####')
 })
